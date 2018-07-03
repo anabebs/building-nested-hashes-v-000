7d35161c0d54ec1a949e5c6cebd47998a6e1e0ad
Ana Bebic <anabebic.x@gmail.com>
2018-07-03 08:59:20 +0000

    automatically backed up by learn

--- spec/bonus_spec.rb
+++ spec/bonus_spec.rb
@@ -9,3 +9,8 @@ describe "#bonus" do
     
   end
 end
+describe "#bonus" do
+   # remove the 'x' from the 'xit' below to activate this test!	   # remove the 'x' from the 'xit' below to activate this test!
+-	xit "uses the []= method to change the hero and heroine's status from alive to dead" do 	+	it "uses the []= method to change the hero and heroine's status from alive to dead" do 
+     expect(bonus[:montague][:hero][:status]).to eq("dead")	     expect(bonus[:montague][:hero][:status]).to eq("dead")
+     expect(bonus[:capulet][:heroine][:status]).to eq("dead")	     expect(bonus[:capulet][:heroine][:status]).to eq("dead"
